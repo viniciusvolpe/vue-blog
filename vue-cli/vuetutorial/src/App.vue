8c6d1ab8038e56b9a0e741bb9cc12859a70b51a9
Vinicius Volpe <vinicius.volpe@arctouch.com>
2020-03-03 15:24:20 -0300

    lesson 19 - nesting components

--- vue-cli/vuetutorial/src/App.vue
+++ vue-cli/vuetutorial/src/App.vue
@@ -1,21 +1,21 @@
  <template>
   <div>
     <h1>{{title}}</h1>
-    <p>{{greeting()}}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+import Ninjas from "./Ninjas.vue";
+
 export default {
+  components: {
+    ninjas: Ninjas
+  },
   data() {
     return {
-      title: "First vue file"
+      title: "Ninja App"
     };
-  },
-  methods: {
-    greeting() {
-      return "Heey";
-    }
   }
 };
 </script>
---
+++ vue-cli/vuetutorial/src/Ninjas.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="ninja in ninjas" :key="ninja">{{ninja}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ninjas: ["Yoshi", "Mario"]
+    };
+  }
+};
+</script>
+<style>
+</style>
