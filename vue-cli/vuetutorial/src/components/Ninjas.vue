761eb02f68f2fd957bf434295872e81107897e65
Vinicius Volpe <vinicius.volpe@arctouch.com>
2020-03-03 16:40:59 -0300

    lesson 26 - lifecycle hooks

--- vue-cli/vuetutorial/src/components/Ninjas.vue
+++ vue-cli/vuetutorial/src/components/Ninjas.vue
@@ -18,6 +18,24 @@ export default {
   },
   data() {
     return {};
+  },
+  beforeCreate() {
+    console.log("before create");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("before mount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("before update");
+  },
+  updated() {
+    console.log("updated");
   }
 };
 </script>
