1987a956e53baf1b00d3330c9ad9f3ea1f018a44
Vinicius Volpe <vinicius.volpe@arctouch.com>
2020-03-04 14:24:28 -0300

    Config libraries

--- vue-cli/vuetutorial/src/App.vue
+++ vue-cli/vuetutorial/src/App.vue
@@ -1,72 +1,7 @@
-<template>
-  <div>
-    <!-- <app-header v-bind:title="title" @changeTitle="updateTitle($event)" />
-    <app-ninjas v-bind:ninjas="ninjas" />
-    <app-footer v-bind:title="title" />-->
-    <!-- <form-helper>
-      <div slot="form-header">
-        <h3>Form header</h3>
-        <p>Form info</p>
-      </div>
-      <div slot="form-fields">
-        <input type="text" name="name" placeholder="Name" />
-        <input type="text" name="password" placeholder="Password" />
-      </div>
-      <div slot="form-controls">
-        <button type="submit" @click="handleSubmit">Submit</button>
-      </div>
-    </form-helper>-->
-    <keep-alive>
-      <component :is="component" />
-    </keep-alive>
-    <button @click="showForm('one')">Form one</button>
-    <button @click="showForm('two')">Form two</button>
-  </div>
-</template>
+<template> </template>
 
 <script>
-import Header from "./components/Header.vue";
-import Footer from "./components/Footer.vue";
-import Ninjas from "./components/Ninjas.vue";
-import FormHelper from "./components/FormHelper.vue";
-import FormOne from "./components/FormOne.vue";
-import FormTwo from "./components/FormTwo.vue";
-
-export default {
-  components: {
-    "app-header": Header,
-    "app-footer": Footer,
-    "app-ninjas": Ninjas,
-    "form-helper": FormHelper,
-    "form-one": FormOne,
-    "form-two": FormTwo
-  },
-  data() {
-    return {
-      ninjas: [
-        { name: "Ryu", speciality: "Vue Components", show: false },
-        { name: "Crystal", speciality: "HTML Wizardry", show: false },
-        { name: "Hitoshi", speciality: "Click Events", show: false },
-        { name: "Tango", speciality: "Conditionals", show: false },
-        { name: "Kami", speciality: "Webpack", show: false },
-        { name: "Yoshi", speciality: "Data Diggin", show: false }
-      ],
-      title: "Vue ninjas",
-      component: "form-one"
-    };
-  },
-  methods: {
-    updateTitle(newTitle) {
-      this.title = newTitle;
-    },
-    handleSubmit() {
-      console.log("submitted");
-    },
-    showForm(form) {
-      this.component = `form-${form}`;
-    }
-  }
-};
+export default {};
 </script>
 
 <style scoped></style>
--- vue-cli/vuetutorial/src/main.js
+++ vue-cli/vuetutorial/src/main.js
@@ -1,7 +1,14 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
+import VueResource from "vue-resource";
+import router from "./router";
 import App from "./App.vue";
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
   el: "#app",
-  render: h => h(App)
+  render: h => h(App),
+  router
 });
