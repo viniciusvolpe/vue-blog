c9e4a73fe94636c9320cc886c86d4eb68522abf9
Vinicius Volpe <vinicius.volpe@arctouch.com>
2020-03-04 14:46:01 -0300

    Creating header component

--- vue-cli/vuetutorial/src/components/Header.vue
+++ vue-cli/vuetutorial/src/components/Header.vue
@@ -1,4 +1,28 @@
 <template>
-  <header></header>
+  <header>
+    <router-link class="home" to="/">Home</router-link>
+    <router-link to="/add">New Post</router-link>
+  </header>
 </template>
-<style scoped></style>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  margin: -8px;
+}
+a {
+  text-decoration: none;
+  padding: 10px;
+}
+.home {
+  background: #444;
+  color: #eee;
+}
+a:not(.home) {
+  color: #444;
+  border: 1px solid #a4ceff;
+  border-radius: 5px;
+  padding: 5px;
+  margin: 5px;
+}
+</style>
