5080998711f2dd54d0c0ab7feecd3feab9f95326
Vinicius Volpe <vinicius.volpe@arctouch.com>
2020-03-04 14:17:38 -0300

    Cleaning up porject to v2

--- vue-cli/vuetutorial/src/main.js
+++ vue-cli/vuetutorial/src/main.js
@@ -1,9 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-// import Ninjas from "./Ninjas.vue";
-// Vue.component("ninjas", Ninjas);
-
-export const bus = new Vue();
 
 new Vue({
   el: "#app",
