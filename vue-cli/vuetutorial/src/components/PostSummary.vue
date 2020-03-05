7be6dfa784d1366ed2793c85ca12e293112b2d51
Vinicius Volpe <vinicius.volpe@arctouch.com>
2020-03-05 09:43:25 -0300

    Layout adjustments

--- vue-cli/vuetutorial/src/components/PostSummary.vue
+++ vue-cli/vuetutorial/src/components/PostSummary.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="post">
-    <img :src="post.image" />
+    <div class="image" :style="{background: `url('${post.image}') center no-repeat`}"></div>
+    <!-- <img :src="post.image" /> -->
     <div>
       <post-title>{{ post.title }}</post-title>
       <subtitle>{{ post.author }}</subtitle>
@@ -27,11 +28,12 @@ export default {
 .post {
   display: flex;
   background: #f9f9f9;
-  padding: 10px;
   margin-bottom: 20px;
 }
-.post img {
-  max-width: 60%;
+.image {
+  min-width: 400px;
+  height: 200px;
   margin-right: 20px;
+  background-size: cover !important;
 }
 </style>
